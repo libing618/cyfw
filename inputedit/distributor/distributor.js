a5e963abda2138f5ec8ac7f68eeb7d1357f17436
libing618 <997098144@qq.com>
2018-05-30 19:25:24 +0800

    1805036

--- inputedit/distributor/distributor.js
+++ inputedit/distributor/distributor.js
@@ -1,63 +1,51 @@
 //分销招募
 const AV = require('../../libs/leancloud-storage.js');
 const { checkRols } =  require('../../model/initForm');;
-const { i_thumb,i_chooseAd } = require('../import/impedit');
+const {hTabClick} = require('../../libs/util.js');
+const {f_modalRecordView} = require('../../model/controlModal');
 var app = getApp()
 Page({
   data:{
-    navBarTitle: app.roleData.uUnit.uName+'的分销商',
+    pNo: 'distributor',
     pw: app.sysinfo.pw,
+    ht:{
+      navTabs: ['已签约店铺','已解约店铺'],
+      fLength: 2,
+      pageCk: 0
+    },
+    cPage: [new Set(),new Set()],
+    pageData: {},
+    sPages: [{
+      pageName: 'tabDistributor'
+    }],
+    showModalBox: false,
+    animationData: {},
     reqData:[
-      {gname: "agreement", p:'招募文件',t: "p"},
-      {gname: "desc", p: '单位描述', t: "p"},
-      {gname: "thumbnail", p: '图片简介', t: "thumb" },
-      {gname: "aGeoPoint", p: '选择地理位置', t: "chooseAd" },
-      {gname: "address", p: '详细地址', t: "ed"}
-    ],
-    vData: app.roleData.uUnit
+      {gname: "agreement", p:'招募文件',t: "agreeFile"},
+      {gname: "agreeState", p:'合同状态',inclose:false,t:"listsel", aList:['签约',' 解约']},
+      {gname: "shopName", p: '店铺名称', t: "thumb" },
+      {gname: "aGeoPoint", p: '店铺地理位置', t: "chooseAd" },
+      {gname: "address", p: '店铺详细地址', t: "ed"}
+    ]
   },
   distributorLog:{},
   onLoad:function(options){
     var that = this;
     if (checkRols(9,app.roleData.user)) {
-      new AV.Query('manufactor').equalTo('unitId',app.roleData.uUnit.objectId).first().then(manufactor=>{
-        if (manufactor) {
-          that.setData({ vData: manufactor.toJSON() });
+      new AV.Query('distributor').equalTo('unitId',app.roleData.uUnit.objectId).ascending('updatedAt').find().then(channel=>{
+        if (channel) {
+          let fc;
+          channel.forEach(csi=>{
+            fc = csi.toJSON();
+            that.data.pageData[fc.shopId)] = fc;
+            that.data.cPage[fc.agreeState].add(fc.shopId);
+          })
+          that.setData({ vData: manufactor });
           that.distributorLog = manufactor;
         }
       }).catch( console.error);
     };
   },
-  i_thumb:i_thumb,
-  i_chooseAd:i_chooseAd,
-  fSave:function(e){
-    var that = this;
-    var sObject = AV.Object.extend('manufactor');
-    let newmf = new sObject();
-    if (that.distributorLog) {
-      newmf.id = that.distributorLog.id;
-      delete that.distributorLog.id;
-    };
-    newmf.set('unitId',app.roleData.uUnit.objectId);
-    newmf.set('uName',app.roleData.uUnit.uName);
-    newmf.set('adminPhone', app.roleData.user.mobilePhoneNumber);
-    newmf.set('nick', app.roleData.uUnit.nick);
-    newmf.set('title', app.roleData.uUnit.title);
-    newmf.set('afamily', app.roleData.uUnit.afamily);
-    newmf.set('desc', e.detail.value.desc);
-    newmf.set('thumbnail', e.detail.value.thumbnail);
-    newmf.set('aGeoPoint', e.detail.value.aGeoPoint);
-    newmf.set('address', e.detail.value.address);
-    newmf.set('agreement',e.detail.value.agreement);
-    newmf.save().then(()=>{
-      if ( that.distributorLog ){
-        let dLog = AV.Object.extend('distributorLog');
-        let saveLog = new dLog();
-        saveLog.set(that.distributorLog);
-        saveLog.save();
-      }
-      wx.showToast({ title: '分销信息已发布', duration: 2500 });
-      setTimeout(function () { wx.navigateBack({ delta: 1 }) }, 2000);
-    }).catch( console.error );
-  }
+  hTabClick:hTabClick,
+  f_modalRecordView:f_modalRecordView
 })
--- libs/allmenu.js
+++ libs/allmenu.js
@@ -4,7 +4,7 @@ manage:{
   N101:{"tourl": "/inputedit/f_Role/f_Role","mName": "单位信息" },
   N102:{"tourl": "/index/structure/structure","mName": "组织架构" },
   N103:{"tourl": "/index/procedures/procedures","mName": "流程审批" },
-  N104:{"tourl": "/inputedit/distributor/distributor", "mName": "分销招募" },
+  N104:{"tourl": "/inputedit/distributor/distributor", "mName": "分销商" },
   N105:{"tourl": "/inputedit/distribution/distribution","mName": "货架管理" },
   N106:{"tourl": "/inputedit/accounting/accounting", "mName": "账务中心" },
   N107:{"tourl": "/index/rcustomer/rcustomer","mName": "联系客服" },
